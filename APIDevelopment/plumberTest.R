0da3674ceacf6824cd9c30a78d99d4b640354484
wtrombly <will@therealestateexaminer.com>
2022-08-09 11:22:57 -0500

    organizing

--- APIDevelopment/plumberTest.R
+++ APIDevelopment/plumberTest.R
@@ -1,6 +1,6 @@
 library(plumber)
 
-# -- YouTube Tutorial   https://www.youtube.com/watch?v=J0Th2QRZ7Rk
+# -- YouTube Tutorial   https://www.youtube.com/watch?v=J0Th2QRZ7Rk ,     https://www.youtube.com/watch?v=7XDDI0BnDAw
 
 #* Health Check - Is the API running
 #*  @get /health-check
